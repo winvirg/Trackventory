67f3c7fb349266dfe4df9bb9add14dd746100a78
kauthsousa <kauthsousa@gmail.com>
2024-08-19 08:42:00 -0300

    teste de erro

--- src/app/user/home/page.tsx
+++ src/app/user/home/page.tsx
@@ -18,6 +18,14 @@ export default function Home(props:String) {
             <header className={styles.header}>
                 <div className={styles.menu_lateral}>
                     <Image src="/images/logo_branca.png" className={styles.imagem_logo} alt="TrackVentory logo" width={160} height={80} priority/>
+                    <nav className={styles.menu_opcoes}>
+                        <ul>
+                            <li><a href="#home">Home</a></li>
+                            <li><a href="#produtos">Produtos</a></li>
+                            <li><a href="#relatorios">Relatórios</a></li>
+                            <li><a href="#vendas">Minhas Vendas</a></li>
+                        </ul>
+                    </nav>
                 </div>
                 <div className={styles.menu_superior}>
                     <div className={styles.menu_sanduiche}>
@@ -28,9 +36,9 @@ export default function Home(props:String) {
                             <nav className={styles.menu_opcoes}>
                                 <ul>
                                     <li><a href="#home">Home</a></li>
-                                    <li><a href="#about">About</a></li>
-                                    <li><a href="#services">Services</a></li>
-                                    <li><a href="#contact">Contact</a></li>
+                                    <li><a href="#produtos">Produtos</a></li>
+                                    <li><a href="#relatorios">Relatórios</a></li>
+                                    <li><a href="#vendas">Minhas Vendas</a></li>
                                 </ul>
                             </nav>
                         </div>
