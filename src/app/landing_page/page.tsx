d175763fba6ed9ead96d575ad2639e249a382cec
Giovana Carneiro <pessoal.giolopes@gmail.com>
2024-09-17 15:56:56 -0300

    ajustando menu de navegação da landing page

--- src/app/landing_page/page.tsx
+++ src/app/landing_page/page.tsx
@@ -1,17 +1,22 @@
 import styles from "../page.module.css";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Landing_page() {
     return (
         <div className={styles.landing_page}>
             <div className={styles.main_lp}>
-                <nav>
-                    <ul>
-                        <li>SOBRE NÓS</li>
-                        <li>PARCEIROS</li>
-                        <li>CONTATO</li>
-                        <li>LOGIN <i></i></li>
-                    </ul>
-                </nav>
+                <header>
+                    <a><Image src="/images/logo_preta.png" className={styles.imagem_logo} alt="TrackVentory logo" width={100} height={50} priority/></a>
+                    <nav>
+                        <ul>
+                            <li><Link href="#sobre_nos">SOBRE NÓS</Link></li>
+                            <li><Link href="#parceiros">PARCEIROS</Link></li>
+                            <li><Link href="#parceiros">CONTATO</Link></li>
+                            <li><Link href="#parceiros">LOGIN</Link></li>
+                        </ul>
+                    </nav>
+                </header>
             </div>
         </div>
     );
