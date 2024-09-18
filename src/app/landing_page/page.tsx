1330be06195a1c994daa7c93a480e75b66c0f8df
Giovana Carneiro <pessoal.giolopes@gmail.com>
2024-09-18 14:13:48 -0300

    ajustando botão de login

--- src/app/landing_page/page.tsx
+++ src/app/landing_page/page.tsx
@@ -7,13 +7,13 @@ export default function Landing_page() {
         <div className={styles.landing_page}>
             <div className={styles.main_lp}>
                 <header>
-                    <a><Image src="/images/logo_preta.png" className={styles.imagem_logo} alt="TrackVentory logo" width={100} height={50} priority/></a>
+                    <Image src="/images/logo_preta.png" className={styles.imagem_logo} alt="TrackVentory logo" width={100} height={50} priority/>
                     <nav>
                         <ul>
                             <li><Link href="#sobre_nos">SOBRE NÓS</Link></li>
                             <li><Link href="#parceiros">PARCEIROS</Link></li>
-                            <li><Link href="#parceiros">CONTATO</Link></li>
-                            <li><Link href="#parceiros">LOGIN</Link></li>
+                            <li><Link href="#contato">CONTATO</Link></li>
+                            <li className={styles.btn_login}><Link href="../login"><Image src="/images/logar.png" className={styles.imagem_logo} alt="TrackVentory logo" width={30} height={30} priority/>LOGIN</Link></li>
                         </ul>
                     </nav>
                 </header>
